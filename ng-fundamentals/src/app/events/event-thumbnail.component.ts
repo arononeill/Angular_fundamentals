1a2b4343220be59671c478637aeddca35fc38ae6
Aron O'Neill <36157615+arononeill@users.noreply.github.com>
2019-08-15 13:17:10 +0100

    update

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -3,21 +3,24 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 @Component({
     selector: 'event-thumbnail',
     template: `
-    <div class="well hoverwell thumbernail">
+    <div class="well hoverwell thumbnail">
         <!-- angular will lookin the EventsListComponent and expect to find an object called event 
         with the properties such as name, date and time */ -->
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Price: {{event.time}}</div> 
-        <div>Price: \${{event.price}}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div>Price: {{event?.time}}</div> 
+        <div>Price: \${{event?.price}}</div>
+        <div [hidden]="!event?.location">
+            <span>Location: {{event?.location.address}}</span>
+            <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
         </div>
-    </div> 
+        <div [hidden]="!event?.onlineUrl">
+            Online URL: {{event?.onlineUrl}}
+        </div>
+    </div>
     `,
     styles: [`
-        .thumbnail { min-height: 500px; }
+        .thumbnail { min-height: 210px; }
         .pad-left { margin-left: 10px; }
         .well div {color: #bbb; }
     `]
